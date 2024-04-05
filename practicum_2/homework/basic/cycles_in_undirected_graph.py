b348eee8fd76ae4a02968d4d1364e3a8db6395f4
Burganova Renata <st117269@student.spbu.ru>
2024-04-06 00:28:21 +0300

    [renataburganova] HW1 done

--- practicum_2/homework/basic/cycles_in_undirected_graph.py
+++ practicum_2/homework/basic/cycles_in_undirected_graph.py
@@ -1,5 +1,5 @@
 import networkx as nx
-
+import matplotlib.pyplot as plt
 
 TEST_GRAPH_FILES = [
     "graph_1_wo_cycles.edgelist",
@@ -7,17 +7,51 @@ TEST_GRAPH_FILES = [
 ]
 
 
-def has_cycles(g: nx.Graph):
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+def plot_graph(G):
+    options = dict(
+        font_size=12,
+        node_size=500,
+        node_color="white",
+        edgecolors="black",
+    )
+    pos = nx.spring_layout(G)
+    nx.draw_networkx(G, pos, **options)
+    if nx.is_weighted(G):
+        labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    plt.show()
 
-    pass
+
+def has_cycles_dfs_algorithm(G):
+    colour = {}
+    parent = {}
+
+    def dfs_visit(node):
+        colour[node] = 1
+        for neighbor in G.neighbors(node):
+            if colour.get(neighbor, 0) == 0:
+                parent[neighbor] = node
+                for j in G.neighbors(neighbor):
+                    if (colour.get(j, 0) == 1 or colour.get(j, 0) == 2) and j != parent[neighbor]:
+                        return False
+                if not dfs_visit(neighbor):
+                    return False
+        colour[node] = 2
+        return True
+
+    for node in G:
+        if node not in colour:
+            if not dfs_visit(node):
+                return False
+    return True
 
 
 if __name__ == "__main__":
     for filename in TEST_GRAPH_FILES:
-        # Load the graph
-        G = nx.read_edgelist("practicum_2/homework/graph_2.edgelist", create_using=nx.Graph)
-        # Output whether it has cycles
-        print(f"Graph {filename} has cycles: {has_cycles(G)}")
+        G = nx.read_edgelist("C:/IT/algorithms/spbu-fundamentals-of-algorithms/practicum_2/homework/basic/" + filename,
+                             create_using=nx.Graph)
+        plot_graph(G)
+        if has_cycles_dfs_algorithm(G):
+            print(f"Graph {filename} has cycles: NO")
+        else:
+            print(f"Graph {filename} has cycles: YES")
